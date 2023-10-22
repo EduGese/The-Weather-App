7639593c217d1e11ba351055be6a30bd93f75926
EduGese <122921699+EduGese@users.noreply.github.com>
2023-10-22 11:47:00 +0200

    Estructura semantica basica

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,12 +6,16 @@ import { WeatherForecastComponent } from './components/weather-forecast/weather-
 import { WeatherService } from './services/weather.service';
 import { HttpClientModule } from '@angular/common/http';
 import { FormsModule } from '@angular/forms';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    WeatherForecastComponent
+    WeatherForecastComponent,
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
